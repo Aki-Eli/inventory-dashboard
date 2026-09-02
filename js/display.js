7cc1d3d608c11e239df1c787ce071dde03bc9643
Aki-Eli <masterachilles1096@gmail.com>
2026-09-02 18:00:49 +0800

    update 3

--- js/display.js
+++ js/display.js
@@ -6,18 +6,16 @@ const container = document.querySelector('#productList');
 const total = document.querySelector('#totalInventoryValue');
 const low = document.querySelector('#lowStockCount');
 const out = document.querySelector('#outOfStockCount');
-const resultBox = document.querySelector('#resultBox');
 const noResult = document.querySelector('#noResultsMessage');
 
 export function displayProducts(products) {
     if (products.length === 0) {
-        resultBox.style.display = "none";
+        container.innerHTML = '';
         noResult.style.display = "block";
         return;
     }
 
     noResult.style.display = "none";
-    resultBox.style.display = "block";
 
     let html = '';
     const lowcount = countLowStockProducts(products);
@@ -29,7 +27,7 @@ export function displayProducts(products) {
         const status = getStockStatus(stock);
 
         html += `
-        <div class="product-card">
+        <div class="product-card" data-id="${id}">
             <p><strong>${name}</strong></p>
             <p>${category}</p>
             <p>₱${price.toLocaleString()}</p>
